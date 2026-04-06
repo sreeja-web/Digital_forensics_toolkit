5fbb68fe24c818d834697ea360b554b905b2f876
sreeja-web <sreeja.ankannagari@gmail.com>
2026-04-06 14:45:26 +0530

    Added auth file

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import streamlit as st
 import json
 import hashlib
@@ -49,56 +49,3 @@ def signup_user():
         else:
             users[username] = hash_password(password)
             save_users(users)
-=======
-import streamlit as st
-import json
-import hashlib
-
-USER_FILE = "users.json"
-
-def load_users():
-    try:
-        with open(USER_FILE, "r") as f:
-            return json.load(f)
-    except:
-        return {}
-
-def save_users(users):
-    with open(USER_FILE, "w") as f:
-        json.dump(users, f)
-
-def hash_password(password):
-    return hashlib.sha256(password.encode()).hexdigest()
-
-def login_user():
-    st.subheader("Login")
-
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        users = load_users()
-        if username in users and users[username] == hash_password(password):
-            st.session_state.username = username
-            return True
-        else:
-            st.error("Invalid credentials")
-
-    return False
-
-
-def signup_user():
-    st.subheader("Signup")
-
-    username = st.text_input("Create Username")
-    password = st.text_input("Create Password", type="password")
-
-    if st.button("Signup"):
-        users = load_users()
-        if username in users:
-            st.warning("User already exists")
-        else:
-            users[username] = hash_password(password)
-            save_users(users)
->>>>>>> 937145374ef1cb54abd7cc95f7939691e2e304be
-            st.success("Account created!")
